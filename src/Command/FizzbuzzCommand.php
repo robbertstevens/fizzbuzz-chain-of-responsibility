f517cff32820d14a6b9920e2afd865a59d1e570f
Robbert Stevens <robbertstevens@gmail.com>
2020-07-18 08:30:08 +0200

    ignore .idea directory

---
+++ src/Command/FizzbuzzCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class FizzbuzzCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('app:fizzbuzz');
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Outputs the FizzBuzz problem')
+            ->addArgument('iterations', InputArgument::OPTIONAL, 'Amount of iterations of fizzbuz have to be shown', 100)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
